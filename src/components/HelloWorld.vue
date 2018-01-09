7d56c9cba5cfeb42c949751e6b9929e26e2445b0
yuzhouzhijia520 <qq0236564043>
2018-01-09 20:41:33 +0800

    提交简单Demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <p>hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import VueResource from 'vue-resource'
+import iView from 'iview'
+import 'iView/dist/styles/iview.css'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+Vue.use(VueResource)
+Vue.use(iView)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
